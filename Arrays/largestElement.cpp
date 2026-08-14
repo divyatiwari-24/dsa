cbdd101aa33d9fd2e2bce70dfc2f14f67266475a
divyatiwari-24 <divya.2407.tiwari@gmail.com>
2026-08-14 17:39:38 +0530

    Solved: largestElement

---
+++ Arrays/largestElement.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;    
+
+int largestElement(vector<int> arr) {
+    int largest = INT_MIN;
+
+    for(int i = 0; i < arr.size(); i++) {
+        if(arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+    return largest;
+}
